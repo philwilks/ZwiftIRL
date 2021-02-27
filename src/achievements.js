534f16cc8aa29180afcbda2dccadc6d15153cc71
Phil Wilks <phil@fourteenfish.com>
2021-02-27 21:01:51 +0000

    Add achievements class

--- src/achievements.js
+++ src/achievements.js
@@ -1,9 +1,84 @@
-import mudUrl from './images/powerups/mud.png';
+import uTurn from './images/achivements/uturn.png';
+import calendar from './images/achivements/calendar.png';
+import finish from './images/achivements/finish.png';
+import toast from './images/achivements/toast.png';
+import volcano from './images/achivements/volcano.png';
+import cameras from './images/achivements/cameras.png';
+import friend from './images/achivements/friend.png';
+import distance from './images/achivements/distance.png';
+import hundredKm from './images/achivements/100km.png';
+import hundredMiles from './images/achivements/100miles.png';
+import rocket1 from './images/achivements/rocket1.png';
+import rocket2 from './images/achivements/rocket2.png';
+import rideOn1 from './images/achivements/rideon1.png';
+import rideOn2 from './images/achivements/rideon2.png';
+import wheelie1 from './images/achivements/wheelie1.png';
+import wheelie2 from './images/achivements/wheelie2.png';
 
 const Achievements = [
     {
-        imageUrl: mudUrl,
-        name: 'Mud'
+        imageUrl: uTurn,
+        name: 'U-turn'
+    },
+    {
+        imageUrl: calendar,
+        name: 'Calendar'
+    },
+    {
+        imageUrl: finish,
+        name: 'Finish'
+    },
+    {
+        imageUrl: toast,
+        name: 'Toast'
+    },
+    {
+        imageUrl: volcano,
+        name: 'Volcano'
+    },
+    {
+        imageUrl: cameras,
+        name: 'Cameras'
+    },
+    {
+        imageUrl: friend,
+        name: 'Friend'
+    },
+    {
+        imageUrl: distance,
+        name: 'Distance'
+    },
+    {
+        imageUrl: hundredKm,
+        name: '100 Km'
+    },
+    {
+        imageUrl: hundredMiles,
+        name: '100 miles'
+    },
+    {
+        imageUrl: rocket1,
+        name: 'Rocket 1'
+    },
+    {
+        imageUrl: rocket2,
+        name: 'Rocket 2'
+    },
+    {
+        imageUrl: rideOn1,
+        name: 'Ride-on 1'
+    },
+    {
+        imageUrl: rideOn2,
+        name: 'Ride-on 2'
+    },
+    {
+        imageUrl: wheelie1,
+        name: 'Wheelie 1'
+    },
+    {
+        imageUrl: wheelie2,
+        name: 'Wheelie 2'
     }
 ]
 
--- src/images.js
+++ src/images.js
@@ -1,14 +1,15 @@
 import { preloadImages } from './helpers'
 
-import topBarBgUrl from './images/topbar.png';
-import powerBgUrl from './images/power.png';
-import bannerBgUrl from './images/banner.png';
+import topBarUrl from './images/topbar.png';
+import powerUrl from './images/power.png';
+import bannerUrl from './images/banner.png';
 import madeWithUrl from './images/madewith.png';
 import mapUrl from './images/map.png';
 import riders1Url from './images/riders1.png';
 import riders2Url from './images/riders2.png';
+import routeUrl from './images/route.png';
 
-const uiImageUrls = [topBarBgUrl, powerBgUrl, bannerBgUrl, madeWithUrl, mapUrl, riders1Url, riders2Url]
+const uiImageUrls = [topBarUrl, powerUrl, bannerUrl, madeWithUrl, mapUrl, riders1Url, riders2Url, routeUrl]
 const uiImageArray = []
 const Images = {}
 
@@ -22,8 +23,7 @@ async function loadImages() {
     Images.map = uiImageArray[4]
     Images.riders1 = uiImageArray[5]
     Images.riders2 = uiImageArray[6]
-    
-    console.log('Loaded images 🌄')
+    Images.route = uiImageArray[7]
 }
 loadImages().then(r => {})
 
