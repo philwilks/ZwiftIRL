33f8c1f34f0b816933a833e505874574a536a249
Phil Wilks <phil@fourteenfish.com>
2021-02-28 00:01:38 +0000

    Fix typo

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ function Footer(props) {
         <div className="text-center py-10 max-w-4xl mx-auto">
             <p className="px-3 mt-8 border-t-2 border-gray-600 pt-6 text-white text-xl mb-2 font-bold">What's new in v2?</p>
             <div className="max-w-2xl mx-auto text-left text-gray-300 mb-6 px-3">
-                <p className="my-3">Thank you so much for all the support for Zwift IRL, it's been great seeing what you've made! In the 5 says since lauch you created over 50,000 images 🤯</p>
+                <p className="my-3">Thank you so much for all the support for Zwift IRL, it's been great seeing what you've made! In the 5 says since launch you created over 50,000 images 🤯</p>
                 <p className="my-3">Here's what's new in v2...</p>
                 <p className="my-3"><strong className="text-white">🏅 Achievements: </strong> As well as the classic route badges you can also now select from 20 achievement badges. The best bit is that you can also customise the title and details for these, so get creative!</p>
                 <p className="my-3"><strong className="text-white">🚀 Auto updating: </strong> The Zwift IRL image now updates as you change the options! </p>
