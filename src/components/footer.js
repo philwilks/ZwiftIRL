19a6ffef3ba1e681fab21ea64fad69add9abc507
Phil Wilks <phil@fourteenfish.com>
2021-03-01 18:19:15 +0000

    Hello lawyers

--- src/components/footer.js
+++ src/components/footer.js
@@ -31,7 +31,7 @@ function Footer(props) {
             </p>
             <p className="px-3 text-gray-300 mb-6 text-sm">Please tweet <a href="https://twitter.com/ZwiftIRL" target="_blank" className="font-semibold text-white">@ZwiftIRL</a> with any feature suggestions or feedback, and tag us in your photos!</p>
             <p className="px-3 mt-6 border-t-2 border-gray-600 pt-8 text-gray-300 text-sm">Made by Phil Wilks. Find me on <a href="https://twitter.com/philwilks" target="_blank" className="text-white font-semibold"><i className="fab fa-twitter text-blue-400 pl-1"></i> Twitter</a> and <a href="https://www.strava.com/athletes/486450" target="_blank" className="text-white font-semibold"><i className="fab fa-strava text-orange pl-1"></i> Strava</a>. Source code for this on <a href="https://github.com/philwilks/ZwiftIRL" target="_blank" className="text-white font-semibold">GitHub</a>.</p>
-            <p className="px-3 text-gray-300 my-2 text-sm">Thanks to all the amazing people at <a href="https://zwift.com" target="_blank" className="font-semibold text-white">Zwift</a> and <a href="https://www.wtrl.racing" target="_blank" className="font-semibold text-white">WTRL</a> for allowing the world to keep racing bikes through lockdown.</p>
+            <p className="px-3 text-gray-300 my-2 text-sm">ZwiftIRL is neither endorsed by nor affiliated with <a href="https://zwift.com" target="_blank" className="font-semibold text-white">Zwift</a>.</p>
         </div>
     );
 }
